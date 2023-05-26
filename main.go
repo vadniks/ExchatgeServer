282e11266cc81e2eae31b330dd0dc76ff3ea4e67
vadniks <whatever@fake.some>
2023-05-27 00:16:38 +0300

    Reindented with spaces

--- main.go
+++ main.go
@@ -2,33 +2,33 @@
 package main
 
 import (
-	"fmt"
-	"net"
-	"os"
+    "fmt"
+    "net"
+    "os"
 )
 
 func errorExit(msg string) {
-	fmt.Fprintln(os.Stderr, msg)
-	os.Exit(1)
+    fmt.Fprintln(os.Stderr, msg)
+    os.Exit(1)
 }
 
 func main() {
-	server, err := net.Listen("tcp", "localhost:8080")
-	if err != nil { errorExit("error listening" + err.Error()) }
+    server, err := net.Listen("tcp", "localhost:8080")
+    if err != nil { errorExit("error listening" + err.Error()) }
 
-	defer server.Close()
+    defer server.Close()
 
-	for {
-		connection, err := server.Accept()
-		if err != nil { errorExit("error accepting " + err.Error()) }
+    for {
+        connection, err := server.Accept()
+        if err != nil { errorExit("error accepting " + err.Error()) }
 
-		buffer := make([]byte, 256)
-		length, err := connection.Read(buffer)
+        buffer := make([]byte, 256)
+        length, err := connection.Read(buffer)
 
-		if err != nil { errorExit("error reading" + err.Error()) }
-		fmt.Println(string(buffer[:length]))
+        if err != nil { errorExit("error reading" + err.Error()) }
+        fmt.Println(string(buffer[:length]))
 
-		_, err = connection.Write([]byte(" World!"))
-		connection.Close()
+        _, err = connection.Write([]byte(" World!"))
+        connection.Close()
 	}
 }
