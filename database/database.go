d8791480aef1748d6bc9b74eaa184cf725b6b8b1
vadniks <whatever@fake.some>
2023-06-15 22:43:24 +0300

    Adding a database

--- database/database.go
+++ database/database.go
@@ -5,19 +5,18 @@ import (
     "ExchatgeServer/utils"
     "context"
     "fmt"
-    "go.mongodb.org/mongo-driver/bson"
     "go.mongodb.org/mongo-driver/mongo"
     "go.mongodb.org/mongo-driver/mongo/options"
 )
 
-const mongoUrl = "mongodb://mongodb:27017/exchatge"
+const mongoUrl = "mongodb://127.0.0.1:27017/exchatge"
 const databaseName = "db"
 const collectionUsers = "users"
 const collectionConversations = "conversations"
 const collectionMessages = "messages"
 
 type User struct { // TODO: store each conversation's encryption key on client's side for each client, store name of each participant of each conversation on the client side
-    id uint
+    id uint32
 }
 
 func Init() {
@@ -31,17 +30,18 @@ func Init() {
     collection := client.Database(databaseName).Collection(collectionUsers)
 
     fmt.Println("a")
-    cursor, err := collection.Find(ctx, bson.D{}) // TODO: test only
-    if err != nil { fmt.Println(err) }
+    count, err := collection.CountDocuments(ctx, User{}) // TODO: test only
     utils.Assert(err == nil)
-    fmt.Println("b")
-    values, err := cursor.Current.Values()
-    utils.Assert(err == nil)
-    size := len(values)
-    fmt.Println(size)
-    if size > 0 { return }
+    fmt.Println(count)
 
-    result, err := collection.InsertOne(ctx, User{0})
-    utils.Assert(err == nil)
-    fmt.Println(result.InsertedID)
+    result := collection.FindOne(ctx, User{10})
+    fmt.Println(result.Err())
+    var user User
+    err = result.Decode(&user)
+    fmt.Println(err)
+    fmt.Println(user.id)
+
+    //result, err := collection.InsertOne(ctx, User{5})
+    //utils.Assert(err == nil)
+    //fmt.Println(result.InsertedID)
 }
