5e3d2a2a366cd5a9b49e3b22f14589df9b35dbe6
vadniks <whatever@fake.some>
2023-06-17 20:15:40 +0300

    Architecture

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -12,9 +12,13 @@ const macSize uint = 16
 const nonceSize uint = 24
 const HashSize uint = 128
 const SignatureSize uint = 64
+const hashDanglingZeroBytesSize uint = 30
+const tokenReferenceSize uint = 68
+const tokenSize uint = 98
 
 //sodium.SignPublicKey{Bytes: []byte{255, 23, 21, 243, 148, 177, 186, 0, 73, 34, 173, 130, 234, 251, 83, 130, 138, 54, 215, 5, 170, 139, 175, 148, 71, 215, 74, 172, 27, 225, 26, 249}}, // goes to clients // TODO: embed public key into client's code
 var signSecretKey = sodium.SignSecretKey{Bytes: []byte{211, 211, 189, 184, 216, 122, 65, 203, 37, 173, 133, 45, 240, 193, 227, 57, 78, 211, 86, 225, 75, 172, 30, 182, 194, 11, 249, 233, 74, 149, 198, 232, 255, 23, 21, 243, 148, 177, 186, 0, 73, 34, 173, 130, 234, 251, 83, 130, 138, 54, 215, 5, 170, 139, 175, 148, 71, 215, 74, 172, 27, 225, 26, 249}}
+var tokenSignKeypair = sodium.MakeSignKP()
 
 func GenerateServerKeys() ([]byte, []byte) {
     serverKeys := sodium.MakeKXKP()
@@ -86,4 +90,25 @@ func CompareWithHash(hash []byte, unhashed []byte) bool {
 func Sign(bytes []byte) []byte {
     utils.Assert(len(bytes) > 0)
     return sodium.Bytes(bytes).Sign(signSecretKey)
+}
+
+//goland:noinspection GoRedundantConversion for (*byte) as without this it won't compile
+func Tokenize(id uint32) (tokenReference []byte, tokenItself []byte) {
+    idBytes := make([]byte, 4)
+    copy(idBytes, unsafe.Slice((*byte) (unsafe.Pointer(&id)), 4))
+
+    signedId := sodium.Bytes(idBytes).Sign(tokenSignKeypair.SecretKey)
+    truncatedHashedSignedId := Hash(signedId)[:HashSize -hashDanglingZeroBytesSize]
+
+    return signedId, truncatedHashedSignedId
+}
+
+func CompareToken(tokenReference []byte, tokenItself []byte) bool {
+    utils.Assert(len(tokenReference) == int(tokenReferenceSize) && len(tokenItself) == int(tokenSize))
+    trueHashedSignedId := make([]byte, HashSize)
+
+    copy(trueHashedSignedId, tokenItself)
+    for i := HashSize - hashDanglingZeroBytesSize; i < HashSize; i++ { trueHashedSignedId[i] = 0 }
+
+    return CompareWithHash(trueHashedSignedId, tokenReference)
 }
--- main.go
+++ main.go
@@ -4,37 +4,12 @@ package main
 import (
     "ExchatgeServer/crypto"
     "fmt"
-    "github.com/jamesruan/sodium"
-    "unsafe"
 )
 
 func main() {
-    skp := sodium.MakeSignKP() // TODO: use hash of a signature of id signing result as a token for verifyin' messages from clients
-
-    msg1 := "test"
-    signed := sodium.Bytes(msg1).Sign(skp.SecretKey)
-    signedHash := crypto.Hash(signed)
-
-    fmt.Println(len(signed) - len(msg1), []byte(msg1), "\t", signed, "\t\n", signedHash[:128 - 30]) // 64 bytes for signature
-
-    msg2, err := signed.SignOpen(skp.PublicKey)
-    fmt.Println(string(msg2), err)
-
-    fmt.Println(crypto.CompareWithHash(signedHash, signed))
-
-    //
-
-    const danglingSize = 30 // sodium.PWHashStore hash func produces salty-hash with dangling zeroes, cut them off
-    id := 1
-    idBytes := make([]byte, 4)
-    copy(idBytes, unsafe.Slice((*byte) (unsafe.Pointer(&id)), 4))
-    signedId := sodium.Bytes(idBytes).Sign(skp.SecretKey) // stays on server
-    truncatedHashedSignedId := crypto.Hash(signedId)[:crypto.HashSize - danglingSize] // goes to client as a token (or as a fromId (id of a sender) replacement)
-
-    trueHashedSignedId := make([]byte, crypto.HashSize)
-    copy(trueHashedSignedId, truncatedHashedSignedId)
-    for i := crypto.HashSize - danglingSize; i < crypto.HashSize; i++ { trueHashedSignedId[i] = 0 }
-    fmt.Println("aaa", len(truncatedHashedSignedId), crypto.CompareWithHash(trueHashedSignedId, signedId)) // TODO: 98 bytes for a token
+    id := uint32(1)
+    reference, token := crypto.Tokenize(id)
+    fmt.Println("aaa", crypto.CompareToken(reference, token))
 
     // TODO: map client connection encryption key with token making it impossible to tamper token imperceptibly to server as both key an token is uniq for each connection
 
