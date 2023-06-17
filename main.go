825e8e8a866a8d390a522b040792531c9febd1cc
vadniks <whatever@fake.some>
2023-06-17 20:00:51 +0300

    Architecture

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
     signed := sodium.Bytes(msg1).Sign(skp.SecretKey)
     signedHash := crypto.Hash(signed)
 
-    fmt.Println(len(signed) - len(msg1), []byte(msg1), "\t", signed, "\t", signedHash[:128 - 30]) // 64 bytes for signature
+    fmt.Println(len(signed) - len(msg1), []byte(msg1), "\t", signed, "\t\n", signedHash[:128 - 30]) // 64 bytes for signature
 
     msg2, err := signed.SignOpen(skp.PublicKey)
     fmt.Println(string(msg2), err)
@@ -35,6 +35,8 @@ func main() {
     copy(trueHashedSignedId, truncatedHashedSignedId)
     for i := crypto.HashSize - danglingSize; i < crypto.HashSize; i++ { trueHashedSignedId[i] = 0 }
     fmt.Println("aaa", len(truncatedHashedSignedId), crypto.CompareWithHash(trueHashedSignedId, signedId)) // TODO: 98 bytes for a token
+
+    // TODO: map client connection encryption key with token making it impossible to tamper token imperceptibly to server as both key an token is uniq for each connection
 
     //var waitGroup sync.WaitGroup
     //waitGroup.Add(1)
