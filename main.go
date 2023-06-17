3eec467ba7179b79836e153e7aa887b0ca2383cb
vadniks <whatever@fake.some>
2023-06-17 19:31:37 +0300

    Architecture

--- main.go
+++ main.go
@@ -2,18 +2,31 @@
 package main
 
 import (
-    "ExchatgeServer/database"
-    "ExchatgeServer/net"
-    "sync"
+    "ExchatgeServer/crypto"
+    "fmt"
+    "github.com/jamesruan/sodium"
 )
 
 func main() {
-    var waitGroup sync.WaitGroup
-    waitGroup.Add(1)
-    go database.Init(&waitGroup)
+    skp := sodium.MakeSignKP() // TODO: use hash of a signature of id signing result as a token for verifyin' messages from clients
 
-    net.Initialize()
-    net.ProcessClients()
+    msg1 := "test"
+    signed := sodium.Bytes(msg1).Sign(skp.SecretKey)
+    signedHash := crypto.Hash(signed)
 
-    waitGroup.Wait()
+    fmt.Println(len(signed) - len(msg1), []byte(msg1), "\t", signed, "\t", signedHash[:128 - 30]) // 64 bytes for signature
+
+    msg2, err := signed.SignOpen(skp.PublicKey)
+    fmt.Println(string(msg2), err)
+
+    fmt.Println(crypto.CompareWithHash(signedHash, signed))
+
+    //var waitGroup sync.WaitGroup
+    //waitGroup.Add(1)
+    //go database.Init(&waitGroup)
+    //
+    //net.Initialize()
+    //net.ProcessClients()
+    //
+    //waitGroup.Wait()
 }
