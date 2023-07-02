bba6107cb666c77a04e3fac8735b04918b535fe3
vadniks <whatever@fake.some>
2023-07-02 19:30:43 +0300

    Structure & logic

--- main.go
+++ main.go
@@ -2,13 +2,20 @@
 package main
 
 import (
-    "ExchatgeServer/database"
     "ExchatgeServer/net"
+    "fmt"
 )
 
 func main() {
-    database.Init()
-    net.Initialize()
-    net.ProcessClients()
-    database.Destroy()
+    fmt.Println(*net.TakeId())
+    fmt.Println(*net.TakeId())
+    fmt.Println(*net.TakeId(), "a")
+    for i := 0; i < 100; i++ { fmt.Println(*net.TakeId(), i) }
+    net.ReturnId(1)
+    net.ReturnId(2)
+    net.ReturnId(0)
+    //database.Init()
+    //net.Initialize()
+    //net.ProcessClients()
+    //database.Destroy()
 }
---
+++ net/idgen.go
@@ -0,0 +1,49 @@
+
+package net
+
+import (
+    "ExchatgeServer/utils"
+    "math"
+    "math/big"
+    "unsafe"
+)
+
+var ids = func () *big.Int { // bitset, aka map[uint32/*id*/]bool/*taken*/
+    xFalse, _ := bools()
+    xIds := new(big.Int)
+
+    xIds.SetBit(xIds, maxUsersCount, 1)
+    utils.Assert(float64(len(xIds.Bytes())) == math.Ceil(float64(maxUsersCount) / float64(8)) && xIds.Bit(0) == uint(xFalse))
+
+    return xIds
+}()
+
+func bools() (byte, byte) { // returns 0, 1 (false, true)
+    xFalse := false
+    xxFalse := *((*byte) (unsafe.Pointer(&xFalse)))
+    utils.Assert(xxFalse == byte(0))
+
+    xTrue := 1
+    xxTrue := *((*byte) (unsafe.Pointer(&xTrue)))
+    utils.Assert(xxTrue == byte(1))
+
+    return xxFalse, xxTrue
+}
+
+func TakeId() *uint32 { // nillable result
+    xFalse, xTrue := bools()
+
+    for i := uint32(0); i < uint32(maxUsersCount); i++ {
+        if ids.Bit(int(i)) == uint(xFalse) { // if the id hasn't been taken
+            ids.SetBit(ids, int(i), uint(xTrue)) // take it
+            return &i
+        }
+    }
+    return nil
+}
+
+func ReturnId(id uint32) {
+    xFalse, xTrue := bools()
+    utils.Assert(id < maxUsersCount && ids.Bit(int(id)) == uint(xTrue))
+    ids.SetBit(ids, int(id), uint(xFalse))
+}
